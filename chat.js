b5394b26cd8ce59a0399225d042540711c250c4a
Yugkum <109890922+Yugkum@users.noreply.github.com>
2022-12-05 17:57:16 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,15 +3,15 @@
 
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 
 function addUser()
 {
     user_name = document.getElementById("user_name").value;
-    firebase.database().ref("/").child(user_name).update({
-        purpose : "adding user"
-    });
+//     firebase.database().ref("/").child(user_name).update({
+//         purpose : "adding user"
+//     });
      
      localStorage.setItem("user_name", user_name);
 
